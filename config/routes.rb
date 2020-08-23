2993a1a67a33119223319bc9b4912df92453cc8e
Drew Williams <drewwilliams5280@gmail.com>
2020-08-23 11:47:06 -0600

    Makes pets the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'shelters#index'
+  root 'pets#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
