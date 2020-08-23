cfce233370b33f6a88ac1563303c8d24e37ea018
Drew Williams <drewwilliams5280@gmail.com>
2020-08-22 22:57:20 -0600

    Adds root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'shelters#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
