910835c79f1b5f1335defb150703a0f1b4118c07
Brett Turing <bjsherman80@gmail.com>
2020-08-25 23:13:00 -0600

    Created add to favorite link

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,12 @@
 class FavoritesController < ApplicationController
   def index
   end
+
+  def new
+    @pet = Pet.find(params[:pet_id])
+    @pet.favorites.create!(pet_id: params[:pet_id])
+    binding.pry
+    redirect_to "/pets/#{params[:pet_id]}"
+    flash[:notice] = "#{Pet.find(params[:pet_id]).name} has been added to your favorites"
+  end
 end
