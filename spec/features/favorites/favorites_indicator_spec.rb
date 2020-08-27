7f2e9a9d4b58899a7f34be518aaa3ebe642ff927
Brett Turing <bjsherman80@gmail.com>
2020-08-27 16:44:34 -0600

    added test for indicator increment

--- spec/features/favorites/favorites_indicator_spec.rb
+++ spec/features/favorites/favorites_indicator_spec.rb
@@ -15,9 +15,23 @@ RSpec.describe "favorites indicator", type: :feature do #user story 8
         expect(page).to have_content("Favorites: 0")
       end
 
-      it 
+
     end
 
+    it "can create favorite" do
+      shelter_1 = Shelter.create( name: "Drew's Rescue",
+                                  address: "208 Main St.",
+                                  city: "Denver",
+                                  state: "CO",
+                                  zip: 80222,
+                                  )
+      jasper = shelter_1.pets.create!(name: "Jasper", approximate_age: 7, sex: "Male", image_path: "https://dogzone-tcwebsites.netdna-ssl.com/wp-content/uploads/2018/07/pomeranian-price-1.jpg")
+      visit "/pets/#{jasper.id}"
+      click_on "Add to Favorites"
+      expect(current_path).to eq("/pets/#{jasper.id}")
+      expect(page).to have_content("#{jasper.name} has been added to your Favorites!")
+      expect(page).to have_content("Favorites: 1")
+    end
 
 end
 #
