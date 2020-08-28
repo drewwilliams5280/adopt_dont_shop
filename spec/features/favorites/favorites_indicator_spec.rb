84d991cdda2cb03c43c63adba4f45c09144deabb
Brett Turing <bjsherman80@gmail.com>
2020-08-27 18:52:45 -0600

    Refactored user story 11

--- spec/features/favorites/favorites_indicator_spec.rb
+++ spec/features/favorites/favorites_indicator_spec.rb
@@ -58,4 +58,24 @@ RSpec.describe "favorites indicator", type: :feature do #user story 8
        click_on "Blitzen"
        expect(current_path).to eq("/pets/#{blitzen.id}")
      end
+
+     it "can see a favorites indicator" do
+       shelter_1 = Shelter.create( name: "Drew's Rescue",
+                                   address: "208 Main St.",
+                                   city: "Denver",
+                                   state: "CO",
+                                   zip: 80222,
+                                   )
+       jasper = shelter_1.pets.create!(name: "Jasper", approximate_age: 7, sex: "Male", image_path: "https://dogzone-tcwebsites.netdna-ssl.com/wp-content/uploads/2018/07/pomeranian-price-1.jpg")
+       visit "/pets/#{jasper.id}"
+       click_on "Add to Favorites"
+       visit "/pets/#{jasper.id}"
+       within(".clearfix") do
+      click_on "Favorites: 1"
+       end
+      expect(current_path).to eq("/favorites")
+      expect(page).to have_content(jasper.name)
+    end
+
+
 end
