255882ae3c0a351a40be011f4e3b8731ce8faa9b
Brett Turing <bjsherman80@gmail.com>
2020-08-30 14:59:33 -0600

    complete us 21

--- spec/features/applications/index_spec.rb
+++ spec/features/applications/index_spec.rb
@@ -15,7 +15,6 @@ it "can see application info" do
   application.pets << tasha
   visit "/pets/#{jasper.id}"
   click_on("View Applications")
-  save_and_open_page
   click_on("Brett")
   expect(current_path).to eq("/applications/#{application.id}")
   expect(page).to have_content("Name:Brett")
@@ -25,6 +24,19 @@ it "can see application info" do
   expect(page).to have_content("Zip:81301")
   expect(page).to have_content("Phone Number:970-677-45567")
   expect(page).to have_content("I love dogs")
+end
 
+it "can see no applications message" do
+  shelter_1 = Shelter.create( name: "Drew's Rescue",
+                              address: "208 Main St.",
+                              city: "Denver",
+                              state: "CO",
+                              zip: 80222,
+                              )
+  jasper = shelter_1.pets.create!(name: "Jasper", approximate_age: 7, sex: "Male", image_path: "https://dogzone-tcwebsites.netdna-ssl.com/wp-content/uploads/2018/07/pomeranian-price-1.jpg")
+  tasha = shelter_1.pets.create!(name: "Tasha", approximate_age: 4, sex: "Female", image_path: "https://www.thesprucepets.com/thmb/ma-SKxXBI5uvv_H0McPOhfCZajU=/1415x1415/smart/filters:no_upscale()/DobermanPinscher-GettyImages-947977330-4309781e940842368e71ef744caa4f9c.jpg")
+  application = Application.create!(name: "Brett", address: "345 bur st.", city: "Phoenix", state: "CO", zip: 81301, phone_number: "970-677-45567", description: "I love dogs" )
+  visit "pets/#{jasper.id}/applications"
+  expect(page).to have_content("There are no applications for #{jasper.name}.")
 end
 end
