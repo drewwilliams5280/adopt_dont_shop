0ccd33ed6ac46271db73c5fb8561c9fbaf523a0c
Drew Williams <drewwilliams5280@gmail.com>
2020-08-31 21:04:28 -0600

    Adds US29 functionality and test

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "shelters new page", type: :feature do
-  
+
   it "can navigate to New Shelter page" do
 
     visit "/shelters"
@@ -21,6 +21,19 @@ RSpec.describe "shelters new page", type: :feature do
     fill_in "shelter[zip]", with: "84544"
     click_button "Create Shelter"
     expect(page).to have_content("Best Friends")
+
+  end
+
+  it "can give flash message when form isn't completed" do
+
+    visit "/shelters/new"
+    fill_in "shelter[name]", with: "Best Friends"
+    fill_in "shelter[address]", with: "4500 3rd Ave."
+    fill_in "shelter[city]", with: ""
+    fill_in "shelter[state]", with: "CO"
+    fill_in "shelter[zip]", with: "84544"
+    click_button "Create Shelter"
+    expect(page).to have_content("Please fill in the city field.")
 
   end
 
