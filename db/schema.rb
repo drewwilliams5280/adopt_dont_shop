1974c48b0c701be60e1f0baebaf13820f12686fb
Drew Williams <drewwilliams5280@gmail.com>
2020-08-31 22:00:46 -0600

    Adds features and model methods for shelter statistics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_232751) do
+ActiveRecord::Schema.define(version: 2020_09_01_033043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2020_08_28_232751) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
-    t.string "rating"
+    t.integer "rating"
     t.string "content"
     t.string "picture"
     t.bigint "shelter_id"
